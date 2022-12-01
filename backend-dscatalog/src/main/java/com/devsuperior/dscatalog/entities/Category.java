1db17af53170655812a4737ab975d3c0d1d53980
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2022-11-30 22:19:06 -0300

    chore: create a CategoryResources and modify Category to increase jpa

--- backend-dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend-dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,14 +2,19 @@ package com.devsuperior.dscatalog.entities;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_categories")
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
