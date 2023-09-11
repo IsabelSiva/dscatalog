a612fcbccc0d49f42797f59ad3a3922b84000899
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2023-09-11 15:37:37 -0300

    add some annotations into category class

--- src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,7 +1,16 @@
 package com.devsuperior.dscatalog.entities;
 
+import lombok.*;
 
-public class Category {
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
     private Long id;
     private String name;
 }
