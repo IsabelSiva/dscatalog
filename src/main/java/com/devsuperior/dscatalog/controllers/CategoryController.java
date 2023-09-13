a35ed0f7f8589c8bec0ea51755b20cfb7ea5cc76
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2023-09-13 10:41:06 -0300

    update the rest controller to return a dto

--- src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dscatalog.controllers;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +18,9 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = categoryService.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+        List<CategoryDTO> list = categoryService.findAll();
+
         return ResponseEntity.ok().body(list);
     }
 }
