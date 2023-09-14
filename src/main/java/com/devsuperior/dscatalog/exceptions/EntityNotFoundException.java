661eef406ca3dc73f9ecb846e9327b6c1610171a
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2023-09-14 08:38:16 -0300

    refactor exceptions classes

--- src/main/java/com/devsuperior/dscatalog/exceptions/EntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.dscatalog.exceptions;
-
-
-public class EntityNotFoundException extends Exception{
-
-    public EntityNotFoundException(String msg){
-        super(msg);
-    }
-}
