9dd5389f715f16d99741bde1adf4af49c29d0413
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2023-09-18 09:34:50 -0300

    create ProductDTO

---
+++ src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+import jakarta.persistence.Column;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+    private Instant date;
+    @Setter(AccessLevel.NONE)
+    private List<CategoryDTO> categories = new ArrayList<>();
+
+    public ProductDTO(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.description = entity.getDescription();
+        this.price = entity.getPrice();
+        this.imgUrl = entity.getImgUrl();
+        this.date = entity.getDate();
+    }
+
+    public ProductDTO(Product entity, Set<Category> categories) {
+        this(entity);
+        categories.forEach(category -> this.categories.add(new CategoryDTO(category)));
+    }
+}
